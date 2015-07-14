12ac796d65a804861a6f0bebd54ae80d4617964c
Chris Grimmett <chris@grimtech.net>
2015-07-13 17:13:09 -0700

    basic scrape and grab first patron

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -10,22 +10,44 @@ var driver = new webdriver.Builder()
 // module.exports = {
 //     check: function check(user, cb) {
 
-        //var call = {};
+//var call = {};
 
-        console.log('checking');
-        //driver.get('https://www.patreon.com/' + user + '?ty=p');
-        driver.get('https://www.patreon.com/schroedingham?ty=p');
+console.log('checking');
+//driver.get('https://www.patreon.com/' + user + '?ty=p');
+driver.get('https://www.patreon.com/schroedingham?ty=p');
         
 
-        var element = driver.findElement(By.id('creatorTitle'));
-        element.then(function(element) {
-                console.log('then\'d');
-                //return elements.;
-                //assert(elements.length).equalTo(1);
-                return cb(null, element.getAttribute('id'));
-                //return 'fart';
-                
-            });
+//driver.findElements(By.id('userSearchBox'))
+var elementsPromise = driver.findElements(By.css('div.searchShareArea a'));
+
+elementsPromise.then(function(elements) {
+    elements[0].getAttribute("href")
+	.then(function(attribute) {
+	    console.log('attribute is: ' + attribute);
+	});
+});
+
+
+//    .then(function(element, test) {
+//        console.log('got an element');
+//	console.log('length is:', element.length);
+//	var attrib = element[0].getAttribute("href");
+
+//	console.log('the attrib is', attrib);
+	
+
+	
+	//console.dir(element[0]);
+
+	//console.log(element.getAttribute('hidden'));
+	
+	//console.log(element);
+        //return elements.;
+        //assert(elements.length).equalTo(1);
+        //return cb(null, element.getAttribute('id'));
+        //return 'fart';
+        
+//    });
 
         // driver.executeScript("window.scrollTo(0,Math.max(document.documentElement.scrollHeight,document.body.scrollHeight,document.documentElement.clientHeight));");
 
